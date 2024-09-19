07264c3fbb92b92bc804273e9695516462063440
Caiyi Shyu <cai1hsu@outlook.com>
2024-09-19 13:32:40 +0000

    完成作业

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include ".assignment/test.h"
+#include <algorithm>
 
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    std::sort(&result.min, &result.max + 1);
 }
